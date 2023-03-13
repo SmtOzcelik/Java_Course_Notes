529432024a575f2914691321d63d04e0d128dbd8
samet ozcelik <smtab142790@gmail.com>
2023-03-13 10:35:00 +0300

    02/03/2023

---
+++ src/day02_variables/C01_variables.java
@@ -0,0 +1,16 @@
+package day02_variables;
+
+public class C01_variables {
+    public static void main(String[] args) {
+
+        int            not               =                 60    ;
+        //data türü    variable ismi    assigment sign      değer//
+
+        int not1=70;
+        not1=90;
+
+        int ort=(not+not1)/2;
+        System.out.println(ort);
+
+    }
+}
---
+++ src/day02_variables/C02_variables.java
@@ -0,0 +1,16 @@
+package day02_variables;
+
+public class C02_variables {
+    public static void main(String[] args) {
+
+        int sayi=10;
+        boolean guzelMi=true;
+                char yas='9';
+
+        System.out.println(sayi);
+        System.out.println("sayi");
+
+
+    }
+
+}
