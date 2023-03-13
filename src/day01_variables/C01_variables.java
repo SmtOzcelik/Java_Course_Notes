6eee54bca7aeef3d24fa37cae8107a67bcdc66d7
samet ozcelik <smtab142790@gmail.com>
2023-03-13 10:32:09 +0300

    01/03/2023

---
+++ src/day01_variables/C01_variables.java
@@ -0,0 +1,8 @@
+package day01_variables;
+
+public class C01_variables {
+
+    public static void main(String[] args) {
+        System.out.println("rabbim senden gelecek her turlu hayra muhtacım");
+    }
+}
