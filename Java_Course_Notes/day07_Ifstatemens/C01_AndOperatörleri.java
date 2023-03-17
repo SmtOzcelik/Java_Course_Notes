c5598142ae603249b9430bb6b5da39ef9aa1a042
samet ozcelik <smtab142790@gmail.com>
2023-03-17 11:57:01 +0300

    05/03/2023

---
+++ Java_Course_Notes/day07_Ifstatemens/C01_AndOperatörleri.java
@@ -0,0 +1,20 @@
+package day07_Ifstatemens;
+
+public class C01_AndOperatörleri {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=15;
+        int c=20;
+
+        System.out.println(a>0 && b<20 && c>=20);//--> 1*1*1 ise 1  true
+        System.out.println(a<0 && b<20 && c>=20);//--> 0*1*1 ise 0 false
+
+
+
+
+
+
+
+    }
+}
