b2e2a4b45f7016a34f2f71ffc9bf8db541feaa01
samet ozcelik <smtab142790@gmail.com>
2023-03-17 11:48:11 +0300

    05/03/2023

---
+++ src/day05_mat/C01_pre_post.java
@@ -0,0 +1,27 @@
+package day05_mat;
+
+public class C01_pre_post {
+    public static void main(String[] args) {
+
+        int sayi1=11;
+        int sayi2=sayi1+5;// sayi2-->16
+
+        // pre veya posti increment veya decrement
+        //sadece ++ ve -- için geçerlidir.
+        //bu 2 işlem sayıdan önce veya sonra yazılmasında farklı 2 işleyiş olur.
+
+        int sayi3=sayi2++;// 1- sayi2 sayi3 e ata sayi3=16
+                           // 2-sayi2 bir artır sayi2=17
+
+        System.out.println("sayi3="+sayi3);
+
+        int sayi4=++sayi1; // 1- sayi1 değeri 1 artır. sayi1=12
+                            // 2- sayi1 değeri sayı4 e atanacak sayi4=12
+
+        System.out.println("sayi4="+sayi4);
+        System.out.println("sayi1="+sayi1);
+        System.out.println("sayi2="+sayi2);
+
+
+    }
+}
