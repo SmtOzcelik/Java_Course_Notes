56cf97a87a0e75fa82d089cfa99eb313a135c84f
samet ozcelik <smtab142790@gmail.com>
2023-03-18 14:42:40 +0300

    07/03/2023

---
+++ src/day07_Ifstatemens/C03_IfStatemens.java
@@ -0,0 +1,17 @@
+package day07_Ifstatemens;
+
+public class C03_IfStatemens {
+    public static void main(String[] args) {
+
+        int d=-23;
+
+        // eğer süslü parantes { kullanmaz isek if sadece kendinden sonra gelen
+        // şarta bakar inceler sonrasnda ki şart bağımsız çalışır yazdırır.
+
+        if (d>0)
+            System.out.println("sayi pozitiftir.");
+        System.out.println("sayı pozitif kalacaktır.");
+
+
+
+}}
