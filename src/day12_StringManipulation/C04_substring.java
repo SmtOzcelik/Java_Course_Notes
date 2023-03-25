d8c5aefc11834bac3921cff924d1ed84aaf0cfb8
samet ozcelik <smtab142790@gmail.com>
2023-03-25 10:22:04 +0300

    13/03/2023

---
+++ src/day12_StringManipulation/C04_substring.java
@@ -0,0 +1,16 @@
+package day12_StringManipulation;
+
+public class C04_substring {
+    public static void main(String[] args) {
+
+        String str="Java cok yasa";
+        System.out.println(str.substring(3,4));// a  3 den sonra 4 e kadar
+        System.out.println(str.substring(5,7));//co  5 den sonra 7 e kadar
+        System.out.println(str.length());// 13 karakterli
+        System.out.println(str.substring(3,3)); // "" hiç
+        System.out.println("===="); // ==== yazdırır her zamanki gibi
+
+
+
+    }
+}
