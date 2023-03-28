15238199ef793fdb62ad378e27c34b085cd2f4aa
samet ozcelik <smtab142790@gmail.com>
2023-03-28 21:53:22 +0300

    15/03/2023

---
+++ src/day14_methodCreation/C06_MethodCreation.java
@@ -0,0 +1,11 @@
+package day14_methodCreation;
+
+public class C06_MethodCreation {
+    public static void main(String[] args) {
+
+        // bi önceki soruda olusturdugumuz metodu burda cagıralım
+
+        System.out.println(C05_MethodCreationReturn.sehirAl());
+        System.out.println(C05_MethodCreationReturn.tarihAl());
+    }
+}
