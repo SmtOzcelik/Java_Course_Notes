2668ddae142dff3de604cad33bda192bda46d2f7
samet ozcelik <smtab142790@gmail.com>
2023-04-05 13:34:14 +0300

    17/03/2023

---
+++ src/day16_forLoop/C02_ForLoopHatalar.java
@@ -0,0 +1,23 @@
+package day16_forLoop;
+
+public class C02_ForLoopHatalar {
+    public static void main(String[] args) {
+
+        /* 1) başlangıc noktasından sonra bitis sartına yaklasmiyorsak  sonsuzluk oluşur
+
+        for (int i = 0; i <-10 ; i++) {
+            System.out.println(i); */
+
+
+        /*  2)eger ilk deger icin bile bitis sarti saglamiyorsa for loop calisir
+          fakat loop bady si hicbir zaman devreye girmez(calismaz)
+
+        i=0 icin for loop calismaya baslar fakat 0>5 e gelince loop broken olur
+         for bady si devreye girmez.
+
+        for (int i = 0; i>5  ; i++) {
+            System.out.println(i); */
+
+
+    }
+}
