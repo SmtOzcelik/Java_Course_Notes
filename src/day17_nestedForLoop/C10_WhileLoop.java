f8a2f5dd6912a7ca28ab32b9afe6f24d910970f5
samet ozcelik <smtab142790@gmail.com>
2023-04-11 10:03:47 +0300

    22/03/2023

---
+++ src/day17_nestedForLoop/C10_WhileLoop.java
@@ -0,0 +1,20 @@
+package day17_nestedForLoop;
+
+public class C10_WhileLoop {
+    public static void main(String[] args) {
+
+        boolean dogruMu=false;
+        int sayi=10;
+
+        while (!dogruMu){
+            if (sayi>20){
+                System.out.println("bu islem tamam");
+                dogruMu=true;
+            }else {
+                System.out.println("istenen sart saglanmadi "+sayi);
+                sayi ++;
+            }
+        }
+
+    }
+}
