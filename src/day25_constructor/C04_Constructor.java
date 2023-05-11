f0cf3fa05a2086dc19b31429d2baaf1848f408cf
samet ozcelik <smtab142790@gmail.com>
2023-05-11 09:48:42 +0300

    04/04/2023

---
+++ src/day25_constructor/C04_Constructor.java
@@ -0,0 +1,12 @@
+package day25_constructor;
+
+public class C04_Constructor {
+    public static void main(String[] args) {
+
+        C03 obje1=new C03();
+
+        obje1.method01();//C03 method calisti
+
+
+    }
+}
