2d49a49714ebc4e83042d5dad0777e859560ae84
samet ozcelik <smtab142790@gmail.com>
2023-05-26 11:54:00 +0300

    07/04/2023

---
+++ src/day35_inheritancedaConstructorKullanimi/DPersonel.java
@@ -0,0 +1,12 @@
+package day35_inheritancedaConstructorKullanimi;
+
+public class DPersonel {
+
+    DPersonel (){
+        System.out.println("Personel parametresiz constroctur ");
+    }
+
+    DPersonel (String isim){
+        System.out.println("Personel parametreli constroctur ");
+    }
+}
