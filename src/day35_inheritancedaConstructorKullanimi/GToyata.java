67899f1cf9b01b6f797e350bee07ee584d60c2b3
samet ozcelik <smtab142790@gmail.com>
2023-05-26 11:54:40 +0300

    07/04/2023

---
+++ src/day35_inheritancedaConstructorKullanimi/GToyata.java
@@ -0,0 +1,14 @@
+package day35_inheritancedaConstructorKullanimi;
+
+public class GToyata {
+
+    GToyata(){
+        System.out.println("GToyota paremetresiz constructor");
+    }
+    GToyata(String isim){
+
+
+        System.out.println("GToyota paremetreli constructor");
+    }
+
+}
