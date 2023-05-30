6de9df5b98a4d686a42d655080b5a8a9117a8a75
samet ozcelik <smtab142790@gmail.com>
2023-05-30 19:10:28 +0300

    09/04/2023

---
+++ src/day37_overriding/C02_Overriding.java
@@ -0,0 +1,20 @@
+package day37_overriding;
+
+public class C02_Overriding {
+    public static void main(String[] args) {
+        /*
+        Farkli classlardaki ayni isim ve signature da method lar olusturabiliriz
+        cunku her bir class kendi icinde calisir.
+         */
+    }
+
+    void ekleme(){
+
+    }
+    void ekleme(int sayi){
+
+    }
+    void ekleme(String str){
+
+    }
+}
