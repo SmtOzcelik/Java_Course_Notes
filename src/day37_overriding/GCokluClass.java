956e8b62425fd6b50ac4d7db9c975b989e3297a8
samet ozcelik <smtab142790@gmail.com>
2023-05-30 19:11:17 +0300

    09/04/2023

---
+++ src/day37_overriding/GCokluClass.java
@@ -0,0 +1,15 @@
+package day37_overriding;
+
+public class GCokluClass {
+
+}
+class ikinciClass{
+    /*
+    Intellij de actigimiz bir class icinde sadece
+    bir public class bulunabilir.
+
+    class sinirlari disinda PUBLİC OLMAYAN baska classlar da olusturabiliriz.
+    ancak projede yazdigimiz class ismini public yapmazsak proje ile class
+    arasinda sorun olacagından CTE olusur
+     */
+}
