7c73dd9c1681ae02bb0968b96a1f88e5963e4a81
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:36:36 +0300

    14/04/2023

---
+++ src/day41_abstractClass_Interface/LX3.java
@@ -0,0 +1,34 @@
+package day41_abstractClass_Interface;
+
+public class LX3 extends KBmw{
+
+    /*
+    x3 class i abstruct silsilesinden sonra gelen ilk concrete class oldugundan
+    tum abstruct parent larda ki, tum abstruct methodlari override etmek zorunda kaldi.
+     */
+
+    @Override
+    public void motor() {
+
+    }
+
+    @Override
+    public void yakit() {
+
+    }
+
+    @Override
+    public void kaporta() {
+
+    }
+
+    @Override
+    public void amblem() {
+
+    }
+
+    @Override
+    public void guvenlik() {
+
+    }
+}
