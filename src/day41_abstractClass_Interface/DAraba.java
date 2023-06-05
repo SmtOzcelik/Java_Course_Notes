894831ca9b638d16935cdbdbc273c62db2376e2e
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:37:29 +0300

    14/04/2023

--- src/day41_abstractClass_Interface/DAraba.java
+++ src/day41_abstractClass_Interface/DAraba.java
@@ -2,6 +2,7 @@ package day41_abstractClass_Interface;
 
 public abstract class DAraba { // abstract olmasi icin class onune yazdik
     public static void main(String[] args) {
+
         /*
          abstract class da main method opsiyonel dir.
          Eger abstract class sadece child class larin tasimak zorunda oldugu
