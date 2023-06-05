d502eafa393a13d7472af2e759139b121a9b05da
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:41:07 +0300

    14/04/2023

--- src/day41_abstractClass_Interface/MX3Dizel.java
+++ src/day41_abstractClass_Interface/MX3Dizel.java
@@ -2,8 +2,7 @@ package day41_abstractClass_Interface;
 
 public abstract class MX3Dizel extends LX3 {
 
-    /*
-    Bir abstruct class concrete bir class i parent edilebilir.
-
+     /*
+       Bir abstruct class concrete bir class i parent edilebilir.
      */
 }
