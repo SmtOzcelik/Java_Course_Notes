ef7a1a2306e0fafdd8483a89b1cdf0762ce784ed
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:35:58 +0300

    14/04/2023

---
+++ src/day41_abstractClass_Interface/GCoralla.java
@@ -0,0 +1,25 @@
+package day41_abstractClass_Interface;
+
+public class GCoralla extends EToyota{
+    @Override
+    public void yakit() {
+
+    }
+
+    @Override
+    public void kaporta() {
+
+    }
+
+    /*
+    Corolla class inin 2 tane parent i var
+    Corolla parent larinin 2 sinin de standartlarina (abstract) uymak zorundadir.
+
+    Concrete bir class parent i olan tum abstract class larda abstract olan methodlari
+    implement etmek ZORUNDADİR.
+    Ancak parent silsilesinde override edilerek concrete yapilan methodlari
+    override etmek zorunda DEGİLDİR
+
+     */
+
+}
