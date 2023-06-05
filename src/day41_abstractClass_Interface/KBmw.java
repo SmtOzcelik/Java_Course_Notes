306c148720e1f0b5cdec0273903ff60268215fdf
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:36:25 +0300

    14/04/2023

---
+++ src/day41_abstractClass_Interface/KBmw.java
@@ -0,0 +1,12 @@
+package day41_abstractClass_Interface;
+
+public abstract class KBmw extends DAraba{
+    /*
+    Bmw abstruct bir class in abstruct child i oldugu icin parent
+    class daki abstract methodlari implement etmek zorunda KALMADI.
+     */
+
+    public abstract void amblem();
+    public abstract void guvenlik();
+
+}
