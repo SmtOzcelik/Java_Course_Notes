ff3d54a96382985c2a3e58371c1e55a18052d212
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:40:14 +0300

    14/04/2023

--- src/day41_abstractClass_Interface/KBmw.java
+++ src/day41_abstractClass_Interface/KBmw.java
@@ -1,6 +1,7 @@
 package day41_abstractClass_Interface;
 
 public abstract class KBmw extends DAraba{
+
     /*
     Bmw abstruct bir class in abstruct child i oldugu icin parent
     class daki abstract methodlari implement etmek zorunda KALMADI.
