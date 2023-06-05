88a9d892b7b8cde897573ebf336fadee75105b09
samet ozcelik <smtab142790@gmail.com>
2023-06-05 10:36:13 +0300

    14/04/2023

---
+++ src/day41_abstractClass_Interface/HA160.java
@@ -0,0 +1,39 @@
+package day41_abstractClass_Interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HA160 extends FMercedes{
+    /*
+    Abstract parent silsilesinden gelen ilk concrete class parent i olan
+    tum class larda ki abstract methodlari concrete hale donusturmak(override etmek)
+    zorundadir.
+
+    Bu kuralin istisnasi parent class lardan herhangi birinde concrete hale
+    donusturulmus, abstract method lardir
+     */
+
+    public static void main(String[] args) {
+
+        HA160 arb1=new HA160();
+        // concrete bir class dan istedigimiz bir objeyi uretibiliriz
+
+        FMercedes arb2=new FMercedes();
+        //mercedes de concrete
+
+       // EToyota arb3=new EToyota();
+       // EToyota arb4=new GCoralla();
+        // Abstruct class lar constructor barindirir ama obje uretemezler
+        // Toyota class i abstruct class oldugundan obje uretilemez
+
+        /*
+
+        List<String>list1 =new List<String>();
+        List<String>list2=new ArrayList<>();
+
+        Abstruct bir class in ozelliklerini tasiyan bir obje olusturmak istedegimizde
+        data turunu istedigimiz abstract class construtor ise child i olan
+        concrete bir class dan seceriz
+         */
+    }
+}
