21d1b052f3112f5623da34fb68e3172f8c696985
samet ozcelik <smtab142790@gmail.com>
2023-06-07 10:22:30 +0300

    16/04/2023

---
+++ src/day43_interface_iterators/M02_Child.java
@@ -0,0 +1,30 @@
+package day43_interface_iterators;
+
+public class M02_Child implements I01_InterfaceBodyOlanMethod {
+    @Override
+    public void motor() {
+
+    }
+
+    @Override
+    public void yakit() {
+
+    }
+
+    @Override
+    public String aku() {
+        return null;
+    }
+    /*
+    parent interface abstract olan 3 methodu implement ettigimizde java
+    itirazini durdurur. Sonradan ekledigimiz defeult veya static keyword
+    ile tanimladigimiz methodlari implement etmememiz sorun olusturmadi.
+     */
+
+    public static void main(String[] args) {
+        I01_InterfaceBodyOlanMethod.direksiyon();
+
+        M02_Child obj=new M02_Child();
+        obj.teker();
+    }
+}
