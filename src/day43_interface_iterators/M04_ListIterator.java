85aa07f28a8acf63a545debc7748b004d2d3d65b
samet ozcelik <smtab142790@gmail.com>
2023-06-07 10:22:56 +0300

    16/04/2023

---
+++ src/day43_interface_iterators/M04_ListIterator.java
@@ -0,0 +1,29 @@
+package day43_interface_iterators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class M04_ListIterator {
+    public static void main(String[] args) {
+        /*
+        ListIterator Iterator interface inin child interface dir.
+        Ama daha cok methoda sahiptir.
+         */
+        List<Integer> liste=new ArrayList<>();
+        liste.add(10);
+        liste.add(20);
+        liste.add(30);
+
+        System.out.println(liste);
+
+        ListIterator lit1=liste.listIterator();
+        Integer temp; // 3 ekmeme yapacagım icin Integer yaptik
+        while (lit1.hasNext()){
+            temp=(Integer) lit1.next()+3;
+            lit1.set(temp);
+
+        }
+        System.out.println(liste);//[13, 23, 33]
+    }
+}
