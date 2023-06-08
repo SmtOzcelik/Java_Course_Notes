72e7aff90f291d0f87fe65b7ee7abc065dc97fef
samet ozcelik <smtab142790@gmail.com>
2023-06-08 10:55:59 +0300

    17/04/2023

--- src/day44_collections/C05_LinkedList_DequeDataTuru.java
+++ src/day44_collections/C05_LinkedList_DequeDataTuru.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 
 public class C05_LinkedList_DequeDataTuru {
     public static void main(String[] args) {
+
         Deque<String> ll4=new LinkedList<>();
         ll4.add("Cavidan");
         ll4.add("Mesut");
