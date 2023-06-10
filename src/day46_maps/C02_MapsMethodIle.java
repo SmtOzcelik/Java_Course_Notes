77cc953b2deb9fff809f41ad0fa8564d45d460cb
samet ozcelik <smtab142790@gmail.com>
2023-06-10 10:35:09 +0300

    19/04/2023

--- src/day46_maps/C02_MapsMethodIle.java
+++ src/day46_maps/C02_MapsMethodIle.java
@@ -7,6 +7,7 @@ public class C02_MapsMethodIle {
     public static void main(String[] args) {
 
         Map<Integer,String> ogrenciMap=ReusableMethods.mapOlustur();
+
         //tum verileri teker teker girmek yerine yan class da
         // olusturdugumuz map i cagirdik  atama yaptik ki kullanabilelim
 
