f21eb3b991ea99be00a1ddb33ef4ed1de28d1559
samet ozcelik <smtab142790@gmail.com>
2023-06-12 21:26:07 +0300

    21/04/2023

--- src/day48_maps/C03_Compute.java
+++ src/day48_maps/C03_Compute.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class C03_Compute {
     public static void main(String[] args) {
+
         Map<String,Integer>harfMapi=new HashMap<>();
         harfMapi.put("A",5);
         harfMapi.put("B",1);
